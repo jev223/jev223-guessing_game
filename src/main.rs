dbfab946986af1387c4c0a48dd0f4b14626ae015
jev223 <79126844+jev223@users.noreply.github.com>
2021-02-22 11:39:26 -0500

    Comparing the Guess to the Secret Number  Finished

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,6 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    // --snip--
-
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
-    }
-}
-/*fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
@@ -28,5 +17,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
-}*/
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
